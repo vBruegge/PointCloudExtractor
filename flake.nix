0c653de4ebc5df415ab6c4a7a650b3e2af03fe16
Lina van Bruegge <ga27caz@mytum.de>
2022-01-31 09:50:33 +0100

    deleted python of flake.nix file

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in {
       devShell.x86_64-linux = pkgs.mkShell rec {
-        name = "pointcloudextractor_lls";
+        name = "geometry-extraction-c++";
 
 	shellHook = ''
           export LD_LIBRARY_PATH=$(nixGLIntel printenv LD_LIBRARY_PATH):$LD_LIBRARY_PATH
@@ -24,7 +24,6 @@
           sfml
           pkg-config
           eigen
-          ]))
         ];
       };
     };
