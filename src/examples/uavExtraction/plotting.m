dc4e8e859c3abdcfabfb33dab3296b65578df3c3
Lina van Bruegge <ga27caz@mytum.de>
2022-02-25 11:08:56 +0100

    added diff to plotting

--- src/examples/uavExtraction/plotting.m
+++ src/examples/uavExtraction/plotting.m
@@ -1,17 +1,16 @@
-in = readmatrix("Results/wing_110mm.dat");
-%in_scan1 = readmatrix("Results/wing_110mm.dat_upper.txt");
-%in_scan2 = readmatrix("Results/wing_110mm.dat_lower.txt");
+in = readmatrix("Results/wing_100mm.dat");
 org = readmatrix("SD7037.dat");
 chord = 294.868073;%360.730499;..
-
-in_scan = [in_scan1; in_scan2];
-in_scan = in_scan/chord;
-in_scan = in_scan + [0.5 0];
+org = org(2:end-1,:);
 
 [tmp, indexMin] = min(in(:,1));
 [tmp2, indexMinOrg] = min(org(:,1));
-%inInterpBottom = interp1(in(1:indexMin,1),in(1:indexMin,2), org(1:indexMinOrg,1));
-%inInterpTop = interp1(in(indexMin:end,1),in(indexMin:end,2), org(indexMinOrg:end,1));
+indices = [99 103];
+in(indices,:) = [];
+inInterpBottom = interp1(in(1:indexMin-1,1),in(1:indexMin-1,2), org(1:indexMinOrg,1));
+inInterpTop = interp1(in(indexMin:end,1),in(indexMin:end,2), org(indexMinOrg:end,1));
+d = gradient(in(:,2), in(:,1));
+
 
 errorBottom = (org(1:indexMinOrg,2) - inInterpBottom);
 errorTop = (org(indexMinOrg:end,2) - inInterpTop);
@@ -25,10 +24,10 @@ figure(1)
 plot(in(:,1), in(:,2))
 hold on
 plot(org(:,1), org(:,2))
-plot(in_scan(:,1), in_scan(:,2))
+plot(in(:,1), d)
 xlabel('x/c')
 ylabel('y/c')
-legend('extracted airfoil', 'original airfoil', 'scan')
+legend('extracted airfoil', 'original airfoil')
 
 figure(2)
 plot(org(1:indexMinOrg,1), abs(errorBottom))
